44d9f7080d21783d80f12597f5bc26451bf4aa54
Harro <forge.harro@pm.me>
2026-02-09 13:17:49 +0100

    New host

---
+++ home/harro/DEDAEROTLT0010.nix
@@ -0,0 +1,74 @@
+{pkgs, ...}: {
+  imports = [
+    ./common
+  ];
+
+  # General config
+  home.username = "harro";
+  home.homeDirectory = "/home/harro";
+  hyprpaperWallpaperPath = ./wallpapers/mountains.jpg;
+
+  nixpkgs.config.allowUnfree = true;
+
+  # Modules
+  homemodules.waybar.enable = false;
+  homemodules.wofi.enable = false;
+  homemodules.firefox.enable = true;
+  homemodules.hypridle.enable = false;
+  homemodules.hyprland.enable = false;
+  homemodules.hyprlock.enable = false;
+  homemodules.hyprpaper.enable = false;
+  homemodules.mako.enable = false;
+  homemodules.tmux.enable = true;
+  homemodules.onedrive.enable = false;
+  homemodules.gemini.enable = true;
+  homemodules.sesh.enable = true;
+  homemodules.kicad.enable = false;
+
+  # Nixvim langs
+  nvim.lang = {
+    bash.enable = true;
+    c.enable = false;
+    docker.enable = true;
+    json.enable = true;
+    markdown.enable = true;
+    nix.enable = true;
+    obsidian.enable = false;
+    python.enable = true;
+    yaml.enable = true;
+    rust.enable = true;
+    arduino.enable = false;
+
+    tex.enable = false;
+  };
+
+  # Packages
+  home.packages = with pkgs; [
+    # Desktop
+    vlc
+    ffmpeg
+
+    # Utils
+    zip
+    unzip
+    zathura
+    wl-clipboard
+    jq
+    appimage-run
+
+    # Workflow
+    lazygit
+    nh
+
+    python312
+    docker-compose
+
+    bottles
+    savvycan
+
+    restream
+  ];
+
+  home.stateVersion = "24.05";
+  programs.home-manager.enable = true;
+}
