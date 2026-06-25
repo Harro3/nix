22850c56c0bc4dd4c154aab40cca3f7a3ca07943
Harro <forge.harro@pm.me>
2026-06-25 11:56:07 +0200

    feat: Nice wallpaper integration

--- programs/noctalia/noctalia.nix
+++ programs/noctalia/noctalia.nix
@@ -4,22 +4,18 @@
   ...
 }:
 {
-  perSystem = { pkgs, ... }: {
+  perSystem = { pkgs, self', ... }: {
     packages.noctalia-shell = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
       inherit pkgs;
 
       settings =
         let
           base = builtins.fromJSON (builtins.readFile ./noctalia.json);
-          wallpapers = pkgs.runCommand "noctalia-wallpapers" { } ''
-            mkdir -p $out
-            cp ${self.wallpaper} $out/
-          '';
         in
         base.settings
         // {
           wallpaper = base.settings.wallpaper // {
-            directory = wallpapers;
+            directory = self'.packages.wallpaperDir;
           };
         };
     };
--- wallpaper/wallpaper.nix
+++ wallpaper/wallpaper.nix
@@ -1,3 +1,24 @@
-{
-  flake.wallpaper = ./mountains.jpg;
+{ ... }: let
+  wallpapers = rec {
+    black-hole = ./black-hole.png;
+    catppuccin-mocha-logo-nix = ./catppuccin-mocha-logo-nix.png;
+    fuji = ./fuji.jpeg;
+    mountains = ./mountains.jpg;
+
+    current = mountains;
+  };
+in {
+  flake.lib.wallpaper = wallpapers;
+
+  perSystem = { pkgs, ... }: {
+    packages.wallpaperDir =
+      pkgs.runCommand "wallpaper-dir" {} ''
+        mkdir -p $out
+        ${builtins.concatStringsSep "\n" (
+          builtins.map
+            (name: "ln -s ${wallpapers.${name}} $out/${name}")
+            (builtins.attrNames wallpapers)
+        )}
+      '';
+  };
 }
