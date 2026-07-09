ccc6643c6c661c619c48238f80ec0ab1ddb4c469
Harro <forge.harro@pm.me>
2026-07-09 10:40:59 +0200

    fixed format on save

--- programs/nixvim/_config/plugins/editor/none-ls.nix
+++ programs/nixvim/_config/plugins/editor/none-ls.nix
@@ -9,6 +9,25 @@
       };
     }
   ];
+
+  extraConfigLua = ''
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client and client.supports_method("textDocument/formatting") then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = args.buf,
+            callback = function()
+              vim.lsp.buf.format({ async = false, bufnr = args.buf })
+            end,
+          })
+        end
+      end,
+    })
+  '';
+
   plugins.none-ls = {
     enable = true;
 
@@ -24,23 +43,5 @@
       sqlfluff.enable = true;
       shfmt.enable = true;
     };
-
-    settings.on_attach = ''
-      function(client, bufnr)
-              if client.supports_method("textDocument/formatting") then
-                vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                  group = augroup,
-                  buffer = bufnr,
-                  callback = function()
-                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-                    -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
-                    vim.lsp.buf.format({ async = false })
-                  end,
-                })
-              end
-              end
-
-    '';
   };
 }
